ee5fa61746643cb7dc7a96c8e05e9ce0450881e1
iamdevonbutler <jpescione@gmail.com>
2017-02-15 12:15:14 -0500

    added obj option values

--- app/app.js
+++ app/app.js
@@ -5,26 +5,40 @@ const laxbro = new ParallaxBro();
 var page1, page2;
 
 page1 = laxbro.addCollection('#collection1', {
-  top: 0,
+  top: 50,
   hide: false,
+  center: true,
 });
 
 page1.addElements({
   '#img1': {
-    top: 0,
-    speed: .5,
-  },
-  '#img2': {
-    top: 400,
-    zIndex: 0,
-    speed: .8,
-    update: {
-      0: () => {
-        this.el.fadeIn();
-      },
-      400: () => {
-        this.el.fadeOut();
-      }
+    top: {
+      100: 100,
+    },
+    // speed: {
+    //   0: 1,
+    //   200: .5,
+    //   300: 0,
+    //   400: -1,
+    // },
+    center: true,
+    hide: {
+      100: false,
+      300: true,
     }
-  }
+  },
+  // '#img2': {
+  //   hide: true,
+  //   top: 800,
+  //   zIndex: 0,
+  //   speed: .1,
+  //   update: {
+  //     0: () => {
+  //       this.el.fadeIn();
+  //     },
+  //     400: () => {
+  //       this.el.fadeOut();
+  //     }
+  //   }
+  // }
 });
