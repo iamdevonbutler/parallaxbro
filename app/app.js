78bff8ffa6bbb4904efb40ba1741aa1d169f0823
iamdevonbutler <jpescione@gmail.com>
2017-02-22 16:42:43 -0500

    added debugger

--- app/app.js
+++ app/app.js
@@ -1,10 +1,10 @@
 const ParallaxBro = require('../lib');
 
-const laxbro = new ParallaxBro('#parallax', 2000);
+const laxbro = new ParallaxBro('#parallax', 2000, {debug: true});
 
 
 const c1 = laxbro.addCollection('#collection1', {
-  hide: true,
+
 });
 
 c1.addElements({
@@ -13,29 +13,34 @@ c1.addElements({
     center: true,
     speed: .6,
   },
-  '[src="images/splatter-intro1.jpg"]': {
-    top: 800,
-    center: true,
-    speed: .8,
-    zIndex: -2,
-  },
+  // '[src="images/splatter-intro1.jpg"]': {
+  //   top: 800,
+  //   center: true,
+  //   speed: .8,
+  //   zIndex: -2,
+  // },
 });
 
 const c2 = laxbro.addCollection('#collection2', {
-  center: true,
+  top: 1100,
 });
 
 c2.addElements({
   '[src="images/project-launch.jpg"]': {
     zIndex: 1,
-    speed: 1.5,
-    top: 600,
+    speed: 1.3,
+    top: 700,
+    center: true,
   },
   '[src="images/splatter-projectlaunch-1.jpg"]': {
     speed: 1,
-
+    center: true,
   },
   '[src="images/splatter-projectlaunch-3.png"]': {
-    speed: 1,
+    top: 100,
+    speed: {
+      0: 1.5,
+      // 900: 0,
+    },
   },
 });
