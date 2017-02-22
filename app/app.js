9c6e5a3ac2b54ca740d5214ab79b9d2fb956a600
iamdevonbutler <jpescione@gmail.com>
2017-02-22 12:47:34 -0500

    bug fixing - throws given invalid selector

--- app/app.js
+++ app/app.js
@@ -4,7 +4,7 @@ const laxbro = new ParallaxBro('#parallax', 2000);
 
 
 const c1 = laxbro.addCollection('#collection1', {
-  // hide: true,
+  hide: true,
 });
 
 c1.addElements({
@@ -13,14 +13,29 @@ c1.addElements({
     center: true,
     speed: .6,
   },
-  '[src="images/splatter_intro1.jpg"]': {
+  '[src="images/splatter-intro1.jpg"]': {
     top: 800,
     center: true,
     speed: .8,
     zIndex: -2,
-  }
+  },
 });
 
 const c2 = laxbro.addCollection('#collection2', {
+  center: true,
+});
 
+c2.addElements({
+  '[src="images/project-launch.jpg"]': {
+    zIndex: 1,
+    speed: 1.5,
+    top: 600,
+  },
+  '[src="images/splatter-projectlaunch-1.jpg"]': {
+    speed: 1,
+
+  },
+  '[src="images/splatter-projectlaunch-3.png"]': {
+    speed: 1,
+  },
 });
